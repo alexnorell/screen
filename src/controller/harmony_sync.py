c1cfeb2c285d99ffe46a565dc74ce69d063100a0
Alex Norell <3845666+alexnorell@users.noreply.github.com>
2021-03-20 12:36:37 -0700

    Debug screen retract failing

--- src/controller/harmony_sync.py
+++ src/controller/harmony_sync.py
@@ -28,8 +28,11 @@ def handle_change(activity):
     activity_string = f"Activity Changed: {activity}"
     logging.info(activity_string)
     previous_activity = _R.get("activity")
+    logging.debug("previous activity %d" % previous_activity)
     if activity in ACTIVITIES and previous_activity not in ACTIVITIES:
         control_screen("extend")
+    elif activity == -1:
+        control_screen("retract")
     elif previous_activity in ACTIVITIES and activity not in ACTIVITIES:
         control_screen("retract")
     _R.set("activity", activity)
